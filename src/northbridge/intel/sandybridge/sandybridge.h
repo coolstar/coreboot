9aee857367e085bcf8a00e6d4f197dd84334f9ed
CoolStar <coolstarorganization@gmail.com>
2016-11-16 09:00:08 -0800

    nb/sandybridge: add proper support for IGD OpRegion with UEFI vbt.bin

--- src/northbridge/intel/sandybridge/sandybridge.h
+++ src/northbridge/intel/sandybridge/sandybridge.h
@@ -244,6 +244,7 @@ unsigned long northbridge_write_acpi_tables(device_t device, unsigned long start
 #if !defined(__PRE_RAM__)
 #include "gma.h"
 int init_igd_opregion(igd_opregion_t *igd_opregion);
+const optionrom_vbt_t *get_uefi_vbt(uint32_t *vbt_len);
 #endif
 
 #endif
